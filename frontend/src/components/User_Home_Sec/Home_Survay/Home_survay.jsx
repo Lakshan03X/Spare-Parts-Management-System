2cbe631f5ba89877b29c1773b2115cf30c9d6070
Lakshan03X <nipunlakshan9255@gmail.com>
2024-10-10 10:39:29 +0530

    bug bix 2

---
+++ frontend/src/components/User_Home_Sec/Home_Survay/Home_survay.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Homenav from "../Home_navbar";
+import Footer from "../Home_nav_footers/home_footer";
+
+function Home_survay() {
+  return (
+    <>
+      <Homenav />
+      <div>this is survey  page</div>
+      <Footer />
+    </>
+  );
+}
+
+export default Home_survay;
